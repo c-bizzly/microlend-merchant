99da95971665676c43de2be4087628a6f5171910
c-bizzly <c-bizzly@users.noreply.github.com>
2025-03-14 22:34:47 +0000

    deploy: 7242280c86648ea2b4a9871474b4310583b20182

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "760190040"
+    serviceWorkerVersion: "2296742185"
   }
 });
